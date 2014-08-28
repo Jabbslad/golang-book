2880fc456124630a0cdf8987a63fb60f2ab65552
Jamie Atkinson <jabbslad@gmail.com>
2014-08-28 23:03:37 +0100

    added perimeter method to Shape interface

--- chapter9/shapes/shapes.go
+++ chapter9/shapes/shapes.go
@@ -21,6 +21,7 @@ type Circle struct {
 
 type Shape interface {
 	area() float64
+	perimeter() float64
 }
 
 type MultiShape struct {
@@ -37,6 +38,16 @@ func (c *Circle) area() float64 {
 	return math.Pi * c.r * c.r
 }
 
+func (c *Circle) perimeter() float64 {
+	return math.Pi * 2 * c.r
+}
+
+func (r *Rectangle) perimeter() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return 2 * l + 2 * w
+}
+
 func (m *MultiShape) area() float64 {
 	total := 0.0
 	for _, v := range m.shapes {
@@ -45,8 +56,18 @@ func (m *MultiShape) area() float64 {
 	return total
 }
 
+func (m *MultiShape) perimeter() float64 {
+	total := 0.0
+	for _, v := range m.shapes {
+		total += v.perimeter()
+	}
+	return total
+}
+
 func main() {
 	r1 := Rectangle{2, 2, 4, 4}
-	m := MultiShape{[]Shape{&r1}}
+	c1 := Circle{2, 3, 5}
+	m := MultiShape{[]Shape{&r1, &c1}}
 	fmt.Println(m.area())
+	fmt.Println(m.perimeter())
 }
